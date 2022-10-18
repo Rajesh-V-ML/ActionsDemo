163e61ac0770076861e0aa0c3ac566e5e2e7b65c
Rajesh V <rajesh.v@maplelabs.com>
2022-10-19 00:32:40 +0530

    pass-test

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func Test_sayHello(t *testing.T) {
 	name := "Rajesh"
-	want := "Hi Rajesh"
+	want := "Hello Rajesh"
 
 	if got := sayHello(name); got != want {
 		t.Errorf("hello() = %q, want %q", got, want)
